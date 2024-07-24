a58a82f2c019ff2f46b3c2c9eef0015213109d4b
Ilya <60238433+veyvolopayli@users.noreply.github.com>
2024-07-24 18:18:15 +0300

    gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,17 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val exposed_version: String by project
-val ktorm_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val exposedVersion: String by project
 
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("io.ktor.plugin") version "2.2.4"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
-    id("com.github.johnrengelman.shadow") version "5.2.0"
+    kotlin("jvm") version "2.0.0"
+    id("io.ktor.plugin") version "2.3.12"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
 }
 
-group = "com.example"
+group = "com.studhunter"
 version = "0.0.1"
+
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
     project.setProperty("mainClassName", mainClass.get())
@@ -27,122 +24,37 @@ repositories {
     mavenCentral()
 }
 
-val sshAntTask = configurations.create("sshAntTask")
+kotlin {
+    jvmToolchain(21)
+}
 
 dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.amazonaws:aws-java-sdk-s3:1.12.429")
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-websockets-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-websockets:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("com.amazonaws:aws-java-sdk-s3:1.12.762")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-websockets-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-websockets:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
     implementation("org.litote.kmongo:kmongo:4.8.0")
     implementation("org.litote.kmongo:kmongo-async:4.8.0")
     implementation("org.litote.kmongo:kmongo-coroutine:4.8.0")
     implementation("org.litote.kmongo:kmongo-reactor:4.8.0")
     implementation("commons-codec:commons-codec:1.15")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("org.postgresql:postgresql:42.5.4")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.postgresql:postgresql:42.7.2")
     implementation("org.apache.commons:commons-email:1.5")
 
     implementation("net.coobird:thumbnailator:0.4.19")
 
     implementation("org.jsoup:jsoup:1.16.1")
-
-//    implementation("org.ktorm:ktorm-core:$ktorm_version")
-//    implementation("org.ktorm:ktorm-support-postgresql:$ktorm_version")
-
-    sshAntTask("org.apache.ant:ant-jsch:1.10.13")
-    implementation(kotlin("stdlib-jdk8"))
-}
-
-tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
-    manifest {
-        attributes(
-            "Main-Class" to application.mainClass.get()
-        )
-    }
-}
-
-ant.withGroovyBuilder {
-    "taskdef"(
-        "name" to "scp",
-        "classname" to "org.apache.tools.ant.taskdefs.optional.ssh.Scp",
-        "classpath" to configurations.get("sshAntTask").asPath
-    )
-    "taskdef"(
-        "name" to "ssh",
-        "classname" to "org.apache.tools.ant.taskdefs.optional.ssh.SSHExec",
-        "classpath" to configurations.get("sshAntTask").asPath
-    )
-}
-
-task("deploy") {
-    dependsOn("clean", "shadowJar")
-    ant.withGroovyBuilder {
-        doLast {
-            val knownHosts = File.createTempFile("knownhosts", "txt")
-            val user = "root"
-            val host = "5.181.255.253"
-            val key = file("keys/studhunter-backend-key")
-            val jarFileName = "com.example.seefood-backend-all.jar"
-            try {
-                "scp"(
-                    "file" to file("build/libs/$jarFileName"),
-                    "todir" to "$user@$host:/root/studhunter",
-                    "keyfile" to key,
-                    "trust" to true,
-                    "knownhosts" to knownHosts
-                )
-                "ssh"(
-                    "host" to host,
-                    "username" to user,
-                    "keyfile" to key,
-                    "trust" to true,
-                    "knownhosts" to knownHosts,
-                    "command" to "mv /root/studhunter/$jarFileName /root/studhunter/studhunter.jar"
-                )
-                "ssh"(
-                    "host" to host,
-                    "username" to user,
-                    "keyfile" to key,
-                    "trust" to true,
-                    "knownhosts" to knownHosts,
-                    "command" to "systemctl stop studhunter"
-                )
-                "ssh"(
-                    "host" to host,
-                    "username" to user,
-                    "keyfile" to key,
-                    "trust" to true,
-                    "knownhosts" to knownHosts,
-                    "command" to "systemctl start studhunter"
-                )
-            } finally {
-                knownHosts.delete()
-            }
-        }
-    }
-}
-
-tasks {
-    create("stage").dependsOn("installDist")
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
